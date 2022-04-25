d5c16c107897c31c24cc24dc3ceea47e93b24205
cong.guo <cong.guo@smartnews.com>
2022-04-26 01:18:44 +0800

    for scylla test

--- dynamodb/script/run.sh
+++ dynamodb/script/run.sh
@@ -1,2 +1,2 @@
-ycsb load dynamodb -P dynamodb/workload/workloada -P dynamodb/conf/dynamodb.properties -threads 20
-ycsb run dynamodb -P dynamodb/workload/workloada -P dynamodb/conf/dynamodb.properties -threads 20
+ycsb load dynamodb -p measurement.interval=both -p target=20000 -P dynamodb/workload/workloada -P dynamodb/conf/dynamodb.properties -threads 100
+ycsb run dynamodb -p measurement.interval=both -p target=20000 -P dynamodb/workload/workloada -P dynamodb/conf/dynamodb.properties -threads 100
